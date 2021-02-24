6e709e7a6839feee91358ceeb3bc8eb1e3e18fc0
ovasquezz <56310550+ovasquezz@users.noreply.github.com>
2021-02-24 19:41:32 +0100

    añadido campos required al formulario

--- resources/views/tutores/create.blade.php
+++ resources/views/tutores/create.blade.php
@@ -18,48 +18,48 @@
           @csrf
           <div class="form-group">    
               <label for="nombre_emp">Empresa:</label>
-              <input type="text" class="form-control" name="nombre_emp" value="{{old('nombre_emp')}}"/>
+              <input type="text" class="form-control" name="nombre_emp" value="{{old('nombre_emp')}}" required/>
               {!!$errors->first('nombre_emp', '<small class="text-danger">:message</small></br>')!!}
           </div>
 
           <div class="form-group">    
               <label for="tipo_identidad">Tipo de documento:</label>
-              <input type="text" class="form-control" name="tipo_identidad" value="{{old('tipo_identidad')}}"/>
+              <input type="text" class="form-control" name="tipo_identidad" value="{{old('tipo_identidad')}}" required/>
               {!!$errors->first('tipo_identidad', '<small class="text-danger">:message</small></br>')!!}
 
           </div>
 
           <div class="form-group">    
               <label for="nie_emp">Documento de identidad:</label>
-              <input type="text" class="form-control" name="nie_emp" value="{{old('nie_emp')}}"/>
+              <input type="text" class="form-control" name="nie_emp" value="{{old('nie_emp')}}" required/>
               {!!$errors->first('nie_emp', '<small class="text-danger">:message</small></br>')!!}
 
           </div>
 
           <div class="form-group">    
               <label for="nombre_tutor">Nombre:</label>
-              <input type="text" class="form-control" name="nombre_tutor" value="{{old('nombre_tutor')}}"/>
+              <input type="text" class="form-control" name="nombre_tutor" value="{{old('nombre_tutor')}}" required/>
               {!!$errors->first('nombre_tutor', '<small class="text-danger">:message</small></br>')!!}
 
           </div>
 
           <div class="form-group">    
               <label for="apellido_tutor">Primer apellido:</label>
-              <input type="text" class="form-control" name="apellido_tutor" value="{{old('apellido_tutor')}}"/>
+              <input type="text" class="form-control" name="apellido_tutor" value="{{old('apellido_tutor')}}" required/>
               {!!$errors->first('apellido_tutor', '<small class="text-danger">:message</small></br>')!!}
 
           </div>
 
           <div class="form-group">    
               <label for="apellido2_tutor">Segundo apellido:</label>
-              <input type="text" class="form-control" name="apellido2_tutor" value="{{old('apellido2_tutor')}}"/>
+              <input type="text" class="form-control" name="apellido2_tutor" value="{{old('apellido2_tutor')}}" required/>
               {!!$errors->first('apellido2_tutor', '<small class="text-danger">:message</small></br>')!!}
 
           </div>
 
           <div class="form-group">    
               <label for="pais_doc_identidad">Pais documento de identidad:</label>
-              <input type="text" class="form-control" name="pais_doc_identidad" value="{{old('pais_doc_identidad')}}"/>
+              <input type="text" class="form-control" name="pais_doc_identidad" value="{{old('pais_doc_identidad')}}" required/>
               {!!$errors->first('pais_doc_identidad', '<small class="text-danger">:message</small></br>')!!}
 
           </div>
@@ -90,19 +90,19 @@
 
           <div class="form-group">    
               <label for="tlf_tutor">Telefono:</label>
-              <input type="text" class="form-control" name="tlf_tutor" value="{{old('tlf_tutor')}}"/>
+              <input type="text" class="form-control" name="tlf_tutor" value="{{old('tlf_tutor')}}" required/>
               {!!$errors->first('tlf_tutor', '<small class="text-danger">:message</small></br>')!!}
 
           </div>
 
           <div class="form-group">    
               <label for="email">Email:</label>
-              <input type="text" class="form-control" name="email" value="{{old('email')}}"/>
+              <input type="text" class="form-control" name="email" value="{{old('email')}}" required/>
               {!!$errors->first('Email', '<small class="text-danger">:message</small></br>')!!}
 
           </div>
 
-          <button type="submit" class="btn btn-primary-outline">Add contact</button>
+          <button type="submit" class="btn btn-primary-outline">Add tutor</button>
       </form>
   </div>
 </div>
